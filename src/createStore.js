03a23fd18b671847af9f95b4a73556827c84e258
Allen Anderson <dande313@gmail.com>
2017-11-06 22:24:46 +0000

    automatically backed up by learn

--- src/createStore.js
+++ src/createStore.js
@@ -2,13 +2,13 @@ export default function createStore(reducer) {
   let state;
   function dispatch(){
 
-    
+
   }
 
   function getState(){
     return state;
   }
-  
+
 }
 
 function render() {
